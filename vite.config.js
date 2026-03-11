df462f41266355208b050121b8fe81ed5a791708
Lillian Gil <lillianmarlee@gmail.com>
2026-03-11 09:28:24 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/',  // ← replace with your exact GitHub repo name
+  base: '/',
 })
